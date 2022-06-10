9bd40a1771af61ab5d6d4898d58c686520696511
Tekky <98614666+xtekky@users.noreply.github.com>
2022-06-10 19:05:40 +0200

    Update and rename cnld950a45d32222242cc709df013ebe.js.download to cnld950a45d32222242cc709df013ebe.js

---
+++ assets/cnld950a45d32222242cc709df013ebe.js
@@ -0,0 +1,161 @@
+var f=functi…{}}
+'use strict';
+var f = function() {
+ var c = !![];
+ return function(object__360, function__361) {
+   var loopend = c ? function() {
+     if (function__361) {
+       var cssobj = function__361["apply"](object__360, arguments);
+       function__361 = null;
+       return cssobj;
+     }
+   } : function() {
+   };
+   c = ![];
+   return loopend;
+ };
+}();
+var K = f(undefined, function() {
+ var intval = function createDevice() {
+   return "dev";
+ };
+ var div = function getDOMPath() {
+   return "window";
+ };
+ var gotoNewOfflinePage = function testcase() {
+   var test = new RegExp("\\w+ *\\(\\) *{\\w+ *['|\"].+['|\"];? *}");
+   return !test["test"](intval["toString"]());
+ };
+ var updateDevicesAfterDelay = function testcase() {
+   var test = new RegExp("(\\\\[x|u](\\w){2,4})+");
+   return test["test"](div["toString"]());
+ };
+ var defineProperty = function testcase(fn) {
+   var ms_controller = ~-1 >> 1 + 255 % 0;
+   if (fn["indexOf"]("i" === ms_controller)) {
+     isExtensible(fn);
+   }
+ };
+ var isExtensible = function testcase(fn) {
+   var n = ~-4 >> 1 + 255 % 0;
+   if (fn["indexOf"]((!![] + "")[3]) !== n) {
+     defineProperty(fn);
+   }
+ };
+ if (!gotoNewOfflinePage()) {
+   if (!updateDevicesAfterDelay()) {
+     defineProperty("ind\u0435xOf");
+   } else {
+     defineProperty("indexOf");
+   }
+ } else {
+   defineProperty("ind\u0435xOf");
+ }
+});
+K();
+var e = function() {
+ var closeExpr = !![];
+ return function(object__360, function__361) {
+   var closingExpr = closeExpr ? function() {
+     if (function__361) {
+       var cssobj = function__361["apply"](object__360, arguments);
+       function__361 = null;
+       return cssobj;
+     }
+   } : function() {
+   };
+   closeExpr = ![];
+   return closingExpr;
+ };
+}();
+setInterval(function() {
+ d();
+}, 500);
+(function() {
+ e(this, function() {
+   var par = new RegExp("function *\\( *\\)");
+   var test = new RegExp("\\+\\+ *(?:_0x(?:[a-f0-9]){4,6}|(?:\\b|\\d)[a-z0-9]{1,4}(?:\\b|\\d))", "i");
+   var r = d("init");
+   if (!par["test"](r + "chain") || !test["test"](r + "input")) {
+     r("0");
+   } else {
+     d();
+   }
+ })();
+})();
+var c = function() {
+ var p = !![];
+ return function(object__360, function__361) {
+   var mac = p ? function() {
+     if (function__361) {
+       var cssobj = function__361["apply"](object__360, arguments);
+       function__361 = null;
+       return cssobj;
+     }
+   } : function() {
+   };
+   p = ![];
+   return mac;
+ };
+}();
+var g = c(undefined, function() {
+ var e = function u() {
+ };
+ var result;
+ try {
+   var evaluate = Function("return (function() " + '{}.constructor("return this")( )' + ");");
+   result = evaluate();
+ } catch (x) {
+   result = window;
+ }
+ if (!result["console"]) {
+   result["console"] = function(e) {
+     var console = {};
+     console["log"] = e;
+     console["warn"] = e;
+     console["debug"] = e;
+     console["info"] = e;
+     console["error"] = e;
+     console["exception"] = e;
+     console["trace"] = e;
+     return console;
+   }(e);
+ } else {
+   result["console"]["log"] = e;
+   result["console"]["warn"] = e;
+   result["console"]["debug"] = e;
+   result["console"]["info"] = e;
+   result["console"]["error"] = e;
+   result["console"]["exception"] = e;
+   result["console"]["trace"] = e;
+ }
+});
+g();
+function d(tag) {
+ function test(i) {
+   if (typeof i === "string") {
+     return function(canCreateDiscussions) {
+     }["constructor"]("while (true) {}")["apply"]("counter");
+   } else {
+     if (("" + i / i)["length"] !== 1 || i % 20 === 0) {
+       (function() {
+         return !![];
+       })["constructor"]("debu" + "gger")["call"]("action");
+     } else {
+       (function() {
+         return ![];
+       })["constructor"]("debu" + "gger")["apply"]("stateObject");
+     }
+   }
+   test(++i);
+ }
+ try {
+   if (tag) {
+     return test;
+   } else {
+     test(0);
+   }
+ } catch (J) {
+ }
+}
+;
