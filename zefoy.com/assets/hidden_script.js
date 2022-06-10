a0b0a40c7373fe9744e62f7bd3e77f23e0b1609f
Tekky <98614666+xtekky@users.noreply.github.com>
2022-06-10 18:21:33 +0200

    Update hidden_script.js

--- zefoy.com/assets/hidden_script.js
+++ zefoy.com/assets/hidden_script.js
@@ -1,47 +1,66 @@
-$(window).on('load', function() {
-    ajaxHRS()
+$(window).on…;
+}
+'use strict';
+$(window).on("load", function() {
+    ajaxHRS();
 });
-var _0x5b22 = ['.wbutton,.spinner-border,.list-group', 'show', 'action', '34679PCJBFa', 'reverse', '645874nqPwDw', 'contentType', 'done', 'attr', 'preventDefault', 'disabled', '1897331fMhVJS', '35nMjANK', 'prop', 'submit', 'join', 'data', 'timeout', '.disableButton', '19WKdDqs', '.error', 'form', 'fail', 'unbind', 'type', '890267blujar', '1374127oOewno', '5443lFtIqA', 'html', 'hide', '1DCBeGP', '880103tUnRiP', '1fowgtd'];
+var expr_list = [".wbutton,.spinner-border,.list-group", "show", "action", "34679PCJBFa", "reverse", "645874nqPwDw", "contentType", "done", "attr", "preventDefault", "disabled", "1897331fMhVJS", "35nMjANK", "prop", "submit", "join", "data", "timeout", ".disableButton", "19WKdDqs", ".error", "form", "fail", "unbind", "type", "890267blujar", "1374127oOewno", "5443lFtIqA", "html", "hide", "1DCBeGP", "880103tUnRiP", "1fowgtd"];
 
-function _0x3b28(_0x418c4b, _0x52191d) {
-    return _0x3b28 = function(_0x5b221c, _0x3b28a9) {
-        _0x5b221c = _0x5b221c - 0x167;
-        var _0x15cb3c = _0x5b22[_0x5b221c];
+function function_1(totalExpectedResults, entrySelector) {
+    return function_1 = function searchSelect2(totalExpectedResults, entrySelector) {
+        totalExpectedResults = totalExpectedResults - 359;
+        var _0x15cb3c = expr_list[totalExpectedResults];
         return _0x15cb3c;
-    }, _0x3b28(_0x418c4b, _0x52191d);
-}(function(_0x66ae0b, _0x5004cf) {
-    var _0x184e04 = _0x3b28;
-    while (!![]) {
+    }, function_1(totalExpectedResults, entrySelector);
+}
+(function(data, oldPassword) {
+    var toMonths = function_1;
+    for (; !![];) {
         try {
-            var _0x2a02bb = -parseInt(_0x184e04(0x173)) + -parseInt(_0x184e04(0x17d)) * parseInt(_0x184e04(0x16c)) + parseInt(_0x184e04(0x179)) * -parseInt(_0x184e04(0x172)) + parseInt(_0x184e04(0x186)) * parseInt(_0x184e04(0x174)) + -parseInt(_0x184e04(0x178)) * -parseInt(_0x184e04(0x177)) + parseInt(_0x184e04(0x17f)) + parseInt(_0x184e04(0x185));
-            if (_0x2a02bb === _0x5004cf) break;
-            else _0x66ae0b['push'](_0x66ae0b['shift']());
+            var userPsd = -parseInt(toMonths(371)) + -parseInt(toMonths(381)) * parseInt(toMonths(364)) + parseInt(toMonths(377)) * -parseInt(toMonths(370)) + parseInt(toMonths(390)) * parseInt(toMonths(372)) + -parseInt(toMonths(376)) * -parseInt(toMonths(375)) + parseInt(toMonths(383)) + parseInt(toMonths(389));
+            if (userPsd === oldPassword) {
+                break;
+            } else {
+                data["push"](data["shift"]());
+            }
         } catch (_0x345fb8) {
-            _0x66ae0b['push'](_0x66ae0b['shift']());
+            data["push"](data["shift"]());
         }
     }
-}(_0x5b22, 0xa8956));
+})(expr_list, 690518);
 
-function revSt(_0x146334) {
-    var _0x3d626d = _0x3b28;
-    return _0x146334['split']('')[_0x3d626d(0x17e)]()[_0x3d626d(0x168)]('');
+function revSt(d) {
+    var gotoNewOfflinePage = function_1;
+    return d["split"]("")[gotoNewOfflinePage(382)]()[gotoNewOfflinePage(360)]("");
 }
 
 function ajaxHRS() {
-    var _0x1797fa = _0x3b28;
-    $(_0x1797fa(0x16e))[_0x1797fa(0x167)](function(_0x37cab4) {
-        var _0x280830 = _0x1797fa;
-        _0x37cab4[_0x280830(0x183)]();
-        var _0xa75d6c = $(this)[_0x280830(0x182)](_0x280830(0x17c)),
-            _0x205f9c = new FormData(this);
-        $(_0x280830(0x16b))['prop']('disabled', !![]);
-        var _0x5c104d = {};
-        _0x5c104d['url'] = _0xa75d6c, _0x5c104d[_0x280830(0x171)] = 'POST', _0x5c104d[_0x280830(0x169)] = _0x205f9c, _0x5c104d[_0x280830(0x180)] = ![], _0x5c104d['cache'] = ![], _0x5c104d['processData'] = ![], _0x5c104d[_0x280830(0x16a)] = 0xc350, $['ajax'](_0x5c104d)[_0x280830(0x181)](function(_0x49bf07) {
-            var _0x4d40df = _0x280830;
-            $('#' + _0xa75d6c)[_0x4d40df(0x175)](atob(decodeURIComponent(revSt(_0x49bf07)))), $(_0x4d40df(0x16e))[_0x4d40df(0x170)](), ajaxHRS(), $(_0x4d40df(0x16b))[_0x4d40df(0x187)](_0x4d40df(0x184), ![]);
-        })[_0x280830(0x16f)](function(_0x236257) {
-            var _0x3090c3 = _0x280830;
-            $('.disableButton')[_0x3090c3(0x187)]('disabled', ![]), ($(_0x3090c3(0x17a))[_0x3090c3(0x176)](), $(_0x3090c3(0x16d))[_0x3090c3(0x17b)]());
+    var target = function_1;
+    $(target(366))[target(359)](function(rpm_traffic) {
+        var now = target;
+        rpm_traffic[now(387)]();
+        var cssLoadClue = $(this)[now(386)](now(380));
+        var data = new FormData(this);
+        $(now(363))["prop"]("disabled", !![]);
+        var params = {};
+        params["url"] = cssLoadClue;
+        params[now(369)] = "POST";
+        params[now(361)] = data;
+        params[now(384)] = ![];
+        params["cache"] = ![];
+        params["processData"] = ![];
+        params[now(362)] = 5E4;
+        $["ajax"](params)[now(385)](function(b) {
+            var titletemplate = now;
+            $("#" + cssLoadClue)[titletemplate(373)](atob(decodeURIComponent(revSt(b))));
+            $(titletemplate(366))[titletemplate(368)]();
+            ajaxHRS();
+            $(titletemplate(363))[titletemplate(391)](titletemplate(388), ![]);
+        })[now(367)](function(canCreateDiscussions) {
+            var titletemplate = now;
+            $(".disableButton")[titletemplate(391)]("disabled", ![]);
+            $(titletemplate(378))[titletemplate(374)]();
+            $(titletemplate(365))[titletemplate(379)]();
         });
     });
-}
+};
